822cd4e58b3af40d4956207473c5f50ecf0417ea
Daniel Jones <simaliagray@gmail.com>
2025-04-14 19:12:04 +0100

    removed static dictionary, bomb count & transforms are now tracked per instance via an inheriting state

--- ExtraBalls/ExtraBalls.cs
+++ ExtraBalls/ExtraBalls.cs
@@ -3,9 +3,12 @@ using System;
 using BepInEx;
 using RoR2;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using MonoMod.Cil;
 using Mono.Cecil.Cil;
+using EntityStates;
 using EntityStates.Bell.BellWeapon;
+using RoR2.Skills;
 
 namespace ExtraBalls
 {
@@ -18,93 +21,28 @@ namespace ExtraBalls
         public const string PluginName = "ExtraBalls";
         public const string PluginVersion = "0.0.0";
 
-        public void OnEnable()
-        {
-            On.EntityStates.Bell.BellWeapon.ChargeTrioBomb.OnExit += ClearCachedInformation;
-        }
-        public void OnDisable()
-        {
-            On.EntityStates.Bell.BellWeapon.ChargeTrioBomb.OnExit -= ClearCachedInformation;
-        }
-        private void ClearCachedInformation(On.EntityStates.Bell.BellWeapon.ChargeTrioBomb.orig_OnExit orig, ChargeTrioBomb self)
-        {
-            if (cachedBellInformation.TryGetValue(self, out _) == true)
-            {
-                cachedBellInformation.Remove(self);
-            }
-            orig(self);
-        }
         public void Awake()
         {
             Log.Init(Logger);
+            ModifySkillDef();
             ApplyILHook();
         }
-        private static Dictionary<ChargeTrioBomb, List<Transform>> cachedBellInformation = new Dictionary<ChargeTrioBomb, List<Transform>>();
-        private static List<Transform> GenerateTransformList(int count, ChargeTrioBomb baseState)
+        private static void ModifySkillDef()
         {
-            if (count == 0)
-            {
-                return null;
-            }
-            var list = new List<Transform>();
-            var baseRadius = 3.8f;
-            float startingPos;
-            int bombsToDistribute = count;
-            int currentRingSize = 8;
-            float radius = baseRadius;
-            while (bombsToDistribute > 0)
-            {
-                float currentStepSize = 2 * Mathf.PI / currentRingSize;
-                if (bombsToDistribute >= currentRingSize)
-                {
-                    startingPos = 0f;
-                }
-                else
-                {
-                    startingPos = 0f - (currentStepSize / 2) * (bombsToDistribute - 1);
-                }
-                float currentPos = startingPos;
-                int remainingBombs = bombsToDistribute;
-                for (int i = 0; i < Mathf.Min(currentRingSize, remainingBombs); i++)
-                {
-                    float x = Mathf.Sin(currentPos) * radius;
-                    float y = Mathf.Cos(currentPos) * radius;
-                    float z = 0f;
-                    Vector3 vector = new Vector3(x, y, z);
-                    GameObject newGameObject = new GameObject();
-                    newGameObject.transform.parent = baseState.transform;
-                    newGameObject.transform.localPosition = vector;
-                    newGameObject.transform.localScale = Vector3.one;
-                    newGameObject.transform.rotation = Quaternion.identity;
-                    Transform newTransform = newGameObject.transform;
-                    list.Add(newTransform);
-                    currentPos += currentStepSize;
-                    bombsToDistribute--;
-                }
-                currentRingSize += 4;
-                radius += 3.8f;
-            }
-            return list;
+            SkillDef skillDef = Addressables.LoadAssetAsync<SkillDef>("RoR2/Base/Bell/BellBodyBellBlast.asset").WaitForCompletion();
+            skillDef.activationState = new SerializableEntityStateType(typeof(ChargeMultiBomb));
         }
+
+        private static Transform GetCurrentBombTransform(ChargeMultiBomb newState, ChargeTrioBomb baseState)
+        {
+            int currentBombIndex = baseState.currentBombIndex;
+            return newState.bombTransforms[currentBombIndex - 1];
+        }
+
         private static void ApplyILHook()
         {
             IL.EntityStates.Bell.BellWeapon.ChargeTrioBomb.FixedUpdate += (il) =>
             {
-                ILCursor c0 = new ILCursor(il);
-                c0.Emit(OpCodes.Ldarg_0);
-                c0.EmitDelegate<Action<ChargeTrioBomb>>((baseState) =>
-                {
-                    if (baseState != null && !cachedBellInformation.TryGetValue(baseState, out _))
-                    {
-                        //this is where you input how many bombs this instance of the attack will fire. set high at your own peril.
-                        int localBombCount = 5;
-                        List<Transform> list = GenerateTransformList(localBombCount, baseState);
-                        float actionSpeedModifier = Mathf.Max(localBombCount / 3, 1);
-                        baseState.timeBetweenPreps /= actionSpeedModifier;
-                        baseState.timeBetweenBarrages /= actionSpeedModifier;
-                        cachedBellInformation.Add(baseState, list);
-                    }
-                });
                 ILCursor c1 = new ILCursor(il);
                 if (c1.TryGotoNext(
                     x => x.MatchLdcI4(3)
@@ -114,18 +52,16 @@ namespace ExtraBalls
                     c1.Emit(OpCodes.Ldarg_0);
                     c1.EmitDelegate<Func<int, ChargeTrioBomb, int>>((originalValue, baseState) =>
                     {
-                        if (baseState != null)
+                        if (baseState is ChargeMultiBomb newState && newState != null)
                         {
-                            List<Transform> list = new List<Transform>();
-                            cachedBellInformation.TryGetValue(baseState, out list);
-                            return list.Count;
+                            return newState.bombTransforms.Count;
                         }
                         return originalValue;
                     });
                 }
                 else
                 {
-                    Log.Error($"c1 failed!");
+                    Log.Error($"IL.EntityStates.Bell.BellWeapon.ChargeTrioBomb.FixedUpdate cursor c1 failed to match!");
                 }
                 ILCursor c2 = new ILCursor(il);
                 if (c2.TryGotoNext(
@@ -135,20 +71,16 @@ namespace ExtraBalls
                     c2.Emit(OpCodes.Ldarg_0);
                     c2.EmitDelegate<Func<Transform, ChargeTrioBomb, Transform>>((originalValue, baseState) =>
                     {
-                        if (baseState != null)
+                        if (baseState is ChargeMultiBomb newState && newState != null)
                         {
-                            int currentBombIndex = baseState.currentBombIndex;
-                            List<Transform> list = new List<Transform>();
-                            cachedBellInformation.TryGetValue(baseState, out list);
-                            //currentBombIndex starts from 1! 
-                            return list[currentBombIndex - 1];
+                            return GetCurrentBombTransform(newState, baseState);
                         }
                         return originalValue;
                     });
                 }
                 else
                 {
-                    Log.Error($"c2 failed!");
+                    Log.Error($"IL.EntityStates.Bell.BellWeapon.ChargeTrioBomb.FixedUpdate cursor c2 failed to match!");
                 }
                 ILCursor c3 = new ILCursor(il);
                 if (c3.TryGotoNext(
@@ -158,15 +90,16 @@ namespace ExtraBalls
                     c3.Emit(OpCodes.Ldarg_0);
                     c3.EmitDelegate<Func<Transform, ChargeTrioBomb, Transform>>((originalValue, baseState) =>
                     {
-                        int currentBombIndex = baseState.currentBombIndex;
-                        List<Transform> list = new List<Transform>();
-                        cachedBellInformation.TryGetValue(baseState, out list);
-                        return list[currentBombIndex-1];
+                        if (baseState is ChargeMultiBomb newState && newState != null)
+                        {
+                            return GetCurrentBombTransform(newState, baseState);
+                        }
+                        return originalValue;
                     });
                 }
                 else
                 {
-                    Log.Error($"c3 failed!");
+                    Log.Error($"IL.EntityStates.Bell.BellWeapon.ChargeTrioBomb.FixedUpdate cursor c3 failed to match!");
                 }
                 ILCursor c4 = new ILCursor(il);
                 if (c4.TryGotoNext(
@@ -176,13 +109,11 @@ namespace ExtraBalls
                     c4.Emit(OpCodes.Ldarg_0);
                     c4.EmitDelegate<Action<ChargeTrioBomb>>((baseState) =>
                     {
-                        if (baseState != null)
+                        if (baseState is ChargeMultiBomb newState && newState != null)
                         {
                             GameObject muzzleFlashPrefab = ChargeTrioBomb.muzzleflashPrefab;
                             GameObject baseObject = baseState.gameObject;
-                            List<Transform> list = new List<Transform>();
-                            cachedBellInformation.TryGetValue(baseState, out list);
-                            Transform transform = list[baseState.currentBombIndex - 1];
+                            Transform transform = GetCurrentBombTransform(newState, baseState);
                             bool transmit = false;
                             if (!baseObject)
                                 return;
@@ -203,9 +134,87 @@ namespace ExtraBalls
                 }
                 else
                 {
-                    Log.Error($"c4 failed!");
+                    Log.Error($"IL.EntityStates.Bell.BellWeapon.ChargeTrioBomb.FixedUpdate cursor c4 failed to match!");
                 }
             };
+        }
+    }
+    public class ChargeMultiBomb : ChargeTrioBomb
+    {
+        public List<Transform> bombTransforms = new List<Transform>();
+        public int bombCount;
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            //bomb count set here
+            bombCount = 5;
+            //time between skill state start and first bomb getting fired (all of these are dependent on attack speed!)
+            base.prepDuration *= 2f;
+            //time between individual bomb preps
+            base.timeBetweenPreps *= 2f;
+            //time between first bomb getting fired and returning to main state (skill is considered finished)
+            base.barrageDuration *= 2f;
+            //time between individual bomb fires
+            base.timeBetweenBarrages *= 2f;
+            GenerateTransforms();
+        }
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+        }
+        public override void OnExit()
+        {
+            base.OnExit();
+        }
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            //prevents brass contraptions from interrupting their own attacks
+            return InterruptPriority.Skill;
+        }
+        public void GenerateTransforms()
+        {
+            if (bombCount == 0)
+            {
+                return;
+            }
+            var baseRadius = 3.8f;
+            float startingPos;
+            int bombsToDistribute = bombCount;
+            int currentRingSize = 8;
+            float radius = baseRadius;
+            while (bombsToDistribute > 0)
+            {
+                float currentStepSize = 2 * Mathf.PI / currentRingSize;
+                if (bombsToDistribute >= currentRingSize)
+                {
+                    startingPos = 0f;
+                }
+                else
+                {
+                    startingPos = 0f - (currentStepSize / 2) * (bombsToDistribute - 1);
+                }
+                float currentPos = startingPos;
+                int remainingBombs = bombsToDistribute;
+                for (int i = 0; i < Mathf.Min(currentRingSize, remainingBombs); i++)
+
+                {
+                    float x = Mathf.Sin(currentPos) * radius;
+                    float y = Mathf.Cos(currentPos) * radius;
+                    float z = 0f;
+                    Vector3 vector = new Vector3(x, y, z);
+                    GameObject newGameObject = new GameObject();
+                    newGameObject.transform.parent = base.transform;
+                    newGameObject.transform.localPosition = vector;
+                    newGameObject.transform.localScale = Vector3.one;
+                    newGameObject.transform.rotation = Quaternion.identity;
+                    Transform newTransform = newGameObject.transform;
+                    bombTransforms.Add(newTransform);
+                    currentPos += currentStepSize;
+                    bombsToDistribute--;
+                }
+                currentRingSize += 4;
+                radius += 3.8f;
+            }
         }
     }
 }
